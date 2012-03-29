21ab1dfa747612f3242f7f3cbf435d2cdef2f758
DrummerHead <drummerhead@gmail.com>
2012-03-29 16:05:52 -0300

    add to github

---
+++ i.js
@@ -0,0 +1,21 @@
+javascript:(function(){
+
+if (typeof ZHJ1bW1lcg === 'undefined') {
+  var jQueryScript = document.createElement('script');
+  jQueryScript.setAttribute('src', '//ajax.googleapis.com/ajax/libs/jquery/1.6.3/jquery.min.js');
+  var caput = document.getElementsByTagName('head');
+  caput[0].appendChild(jQueryScript);
+  ZHJ1bW1lcg = true;
+}
+
+setTimeout(function(){
+$(document).ready(function(){
+/* your jQuery code starts here */
+
+
+
+/* your jQuery code ends here */
+});
+},200);
+
+})();
