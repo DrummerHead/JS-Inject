ba3521660d65fd18e55c73c7d16bc2bbce126fe3
DrummerHead <drummerhead@gmail.com>
2012-05-20 15:07:06 -0300

    Major refactor based on CSS-Inject refactor, now with comment removal

---
+++ js/gedrag.js
@@ -0,0 +1,45 @@
+(function($){
+
+
+
+
+// Start codeMirror
+var editor = CodeMirror.fromTextArea(document.getElementById('codeInput'), {
+  lineNumbers: true,
+  matchBrackets: true
+});
+
+
+// Define DOM areas
+var $form = $('#magia')
+  , $targetLink = $('#a-holder')
+  , $codeInput = $('#codeInput')
+  , $linkName = $('#nome')
+
+
+// Make the form parse JS User Input
+$form.submit(function(e){
+  e.preventDefault();
+
+  var raw = $codeInput.val()
+    , parsedJs = raw
+        .replace(/\/\/.*/g,'')     // Delete commentaries
+        .replace(/'/g,'%27')       // Safe single quotes
+        .replace(/"/g,'%22')       // Safe double quotes
+        .replace(/[\r\n\t]/g,' ')  // No newlines or tabs
+        .replace(/ +/g,' ')        // No extra spaces
+    , nome = $linkName.val()
+    , hasJq = $('#hasJQuery:checked').length
+    , aHref = hasJq ?
+        "javascript:(function(){if(typeof ZHJ1bW1lcg === 'undefined'){var jQueryScript=document.createElement('script');jQueryScript.setAttribute('src','//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js');var caput=document.getElementsByTagName('head');caput[0].appendChild(jQueryScript);ZHJ1bW1lcg=true;}setTimeout(function(){$(document).ready(function(){"+ parsedJs +"});},200);})();"
+          :
+        "javascript:(function(){"+ parsedJs +"})();"
+    , aElement = '<a href="'+aHref+'">'+nome+'</a>'
+
+  $targetLink.empty().append(aElement);
+});
+
+
+
+
+})(jQuery)
---
+++ js/testCase.js
@@ -0,0 +1,6 @@
+// To be used inside the page itself, including this commentary
+
+$("body").css({"background-color":"red"}); // Do a barrel roll
+$('h1').css({'background-color':'green'});
+var texty = "This is so \"wrong\" that I wanna grab a \\ character and put it here";
+$('small').after(texty)
