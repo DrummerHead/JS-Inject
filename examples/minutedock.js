bff338e579fea214a7dcb62c91441c1e02e1b34c
DrummerHead <drummerhead@gmail.com>
2012-06-26 14:59:45 -0300

    Goal discrimination, DOM traversion with Prototype and injection of template timeBar

--- examples/minutedock.js
+++ examples/minutedock.js
@@ -1,4 +1,3 @@
-
 var tempus = new Date()
 
   , miliInADay = 86400000
@@ -17,18 +16,29 @@ var tempus = new Date()
 
 
   , $brief = $$('.brief')
-  , $briefMonthly
   , regexMonthly = /monthly/
+  , regexWeekly = /weekly/
   ;
 
+var makeTimeBar = function(n){
+  var barHTML = '<div class="progress_bar_track"><div class="progress_bar"><div style="width: ' + n + '%" class="progress">&nbsp;</div></div></div>';
+
+  return barHTML
+};
+
 $brief.each(function(el){
-  var briefPeriod = el.select('.normal .period')
-    , briefPeriodText = briefPeriod[0].textContent.strip()
+  var $briefPeriod = el.down('.normal .period')
+    , briefPeriodText = $briefPeriod.textContent.strip()
+    , $progressBarTrack = el.down('.progress_bar_track')
     , isMonth = regexMonthly.test(briefPeriodText)
+    , isWeek = regexWeekly.test(briefPeriodText)
     ;
 
   if(isMonth){
-    console.log('siii carajooo');
+    $progressBarTrack.insert({after : makeTimeBar(77)});
+  }
+  else if(isWeek){
+    $progressBarTrack.insert({after : makeTimeBar(20)});
   }
   else{
     console.log('noooo carajooo');
