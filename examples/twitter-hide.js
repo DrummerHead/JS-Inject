5ed6ca558257d9b4549bbe88762697ec4a926ed8
DrummerHead <drummerhead@gmail.com>
2013-12-03 11:05:07 -0200

    Remove replies to other people on someone's timeline

---
+++ examples/twitter-hide.js
@@ -0,0 +1,15 @@
+/* -  These is a javascript snippet to be used with
+      http://mcdlr.com/js-inject/              - */
+
+
+/* -  Twitter - https://twitter.com/                          - *\
+|* -  Remove replies to other people on someone's timeline,   - *|
+|* -  akin to only seeing what you would see if you followed  - *|
+|* -  that person (to judge whether to follow or not)         - *|
+\* -  v1.0                                                    - */
+
+
+$('head').append('<style>#timeline [data-is-reply-to]{display:none !important}</style>')
+
+
+/* -  /Twitter  - */
