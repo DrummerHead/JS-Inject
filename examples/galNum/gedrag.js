9450a6d19d010d7d0da119a493df4ac813a0967e
DrummerHead <drummerhead@gmail.com>
2014-01-26 17:13:41 -0200

    galNum = link to load more, add links to image

--- examples/galNum/gedrag.js
+++ examples/galNum/gedrag.js
@@ -1,12 +1,12 @@
 var href = 'http://photos.safaribookings.com/library/botswana/xxl/Mokolodi_Nature_Reserve_001.jpg';
-var hrefBar = document.getElementById('hrefBar');
-var from = document.getElementById('from');
-var to = document.getElementById('to');
-var form = document.querySelector('form');
-var main = document.querySelector('main');
+var $hrefBar = document.getElementById('hrefBar');
+var $from = document.getElementById('from');
+var $to = document.getElementById('to');
+var $form = document.querySelector('form');
+var $main = document.querySelector('main');
+var loadShift = 50;
 var hrefBarContent = '';
 var constantHref;
-
 var hrefParts = href.split(/(\d+)/).map(function(part){
   return {
     'part' : part,
@@ -24,45 +24,60 @@ var splitBySelected = function(object){
   return result;
 };
 
-var zeraize = function(number, length){
-  num = number.toString();
+var zeroize = function(number, length){
+  var num = number.toString();
   while(num.length < length){
     num = '0' + num;
   }
   return num;
 };
 
+var createGal = function(from, to){
+  $main.innerHTML = '';
+  var gallery = '<ul id="gal">';
+  var hasZeros = from.toString().match(/^0*/)[0].length > 0;
+
+  for(var i = parseInt(from, 10); i <= parseInt(to, 10); i++){
+    var num = (hasZeros ? zeroize(i, from.length) : i);
+    var src = constantHref[0] + num + constantHref[1];
+    gallery += '<li><img src="' + src + '" alt="' + num + '"><a href="' + src + '">' + src + '</a></li>';
+  }
+  gallery += '</ul>';
+  $main.innerHTML = gallery;
+
+  $main.insertAdjacentHTML('beforeEnd', '<strong id="load-more">Load more</strong>');
+  document.getElementById('load-more').addEventListener('click', function(){
+    window.scrollTo(0, 0);
+    var newFrom = (hasZeros ? zeroize(to, from.length) : to);
+    var newTo = parseInt(to, 10) + loadShift;
+    $from.value = newFrom;
+    $to.value = newTo;
+    createGal(newFrom, newTo);
+  });
+};
+
+
 for(part in hrefParts){
   var I = hrefParts[part];
   hrefBarContent += ( I.isNumber ? '<b data-position="' + part + '">' + I.part + '</b>' : '<span>' + I.part+ '</span>' );
 }
+$hrefBar.innerHTML = hrefBarContent;
+var $numbers = document.querySelectorAll('#hrefBar b');
 
-hrefBar.innerHTML = hrefBarContent;
-var numbers = document.querySelectorAll('#hrefBar b');
-
-for(var i = 0; i < numbers.length; i++){
-  var I = numbers.item(i);
+for(var i = 0; i < $numbers.length; i++){
+  var I = $numbers.item(i);
   I.addEventListener('click', function(e){
-    form.classList.add('visible');
+    $form.classList.add('visible');
     this.parentNode.classList.add('done');
     this.classList.add('selected');
     hrefParts[this.getAttribute('data-position')].isSelected = true;
     constantHref = splitBySelected(hrefParts);
-    from.value = this.textContent;
-    to.value = parseInt(this.textContent, 10) + 50;
+    $from.value = this.textContent;
+    $to.value = parseInt(this.textContent, 10) + loadShift;
   });
 }
 
-form.addEventListener('submit', function(e){
+$form.addEventListener('submit', function(e){
   e.preventDefault();
-  main.innerHTML = '';
-  var gallery = '<ul id="gal">';
-  var zeroes = from.value.match(/^0*/);
-
-  for(var i = parseInt(from.value, 10); i <= parseInt(to.value, 10); i++){
-    src = constantHref[0] + (zeroes[0].length > 0 ? zeraize(i, from.value.length) : i) + constantHref[1];
-    gallery += '<li><img src="' + src + '" alt="' + src + '" title="' + src + '"></li>';
-  }
-  gallery += '</ul>';
-  main.innerHTML = gallery;
+  createGal($from.value, $to.value);
 });
