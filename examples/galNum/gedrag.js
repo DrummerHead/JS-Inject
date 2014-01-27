44018234eaa6b54fbbe406bef05b5b0c04464422
DrummerHead <drummerhead@gmail.com>
2014-01-27 18:44:21 -0200

    Optimize galNum for minification

--- examples/galNum/gedrag.js
+++ examples/galNum/gedrag.js
@@ -1,17 +1,24 @@
+(function(document, window){
+
+
+
+
+var $ = function(element){
+  return document.querySelector(element)
+};
 var href = 'http://photos.safaribookings.com/library/botswana/xxl/Mokolodi_Nature_Reserve_001.jpg';
-var $hrefBar = document.getElementById('hrefBar');
-var $from = document.getElementById('from');
-var $to = document.getElementById('to');
-var $form = document.querySelector('form');
-var $main = document.querySelector('main');
-var loadShift = 50;
+var $from = $('#from');
+var $to = $('#to');
+var $form = $('form');
+var $main = $('main');
+var loadShift = 25;
 var hrefBarContent = '';
 var constantHref;
 var hrefParts = href.split(/(\d+)/).map(function(part){
   return {
-    'part' : part,
-    'isNumber' : ( /^\d+$/.test(part) ? true : false ),
-    'isSelected' : false
+    'prt' : part,
+    'isN' : ( /^\d+$/.test(part) ? true : false ),
+    'isS' : false
   }
 });
 
@@ -19,7 +26,7 @@ var splitBySelected = function(object){
   var result = ['', ''];
   var position = 0;
   for(chunk in object){
-    object[chunk].isSelected ? position = 1 : result[position] += object[chunk].part;
+    object[chunk].isS ? position = 1 : result[position] += object[chunk].prt;
   }
   return result;
 };
@@ -46,7 +53,7 @@ var createGal = function(from, to){
   $main.innerHTML = gallery;
 
   $main.insertAdjacentHTML('beforeEnd', '<strong id="load-more">Load more</strong>');
-  document.getElementById('load-more').addEventListener('click', function(){
+  $('#load-more').addEventListener('click', function(){
     window.scrollTo(0, 0);
     var newFrom = (hasZeros ? zeroize(to, from.length) : to);
     var newTo = parseInt(to, 10) + loadShift;
@@ -59,9 +66,9 @@ var createGal = function(from, to){
 
 for(part in hrefParts){
   var I = hrefParts[part];
-  hrefBarContent += ( I.isNumber ? '<b data-position="' + part + '">' + I.part + '</b>' : '<span>' + I.part+ '</span>' );
+  hrefBarContent += ( I.isN ? '<b data-position="' + part + '">' + I.prt + '</b>' : '<span>' + I.prt + '</span>' );
 }
-$hrefBar.innerHTML = hrefBarContent;
+$('#hrefBar').innerHTML = hrefBarContent;
 var $numbers = document.querySelectorAll('#hrefBar b');
 
 for(var i = 0; i < $numbers.length; i++){
@@ -70,7 +77,7 @@ for(var i = 0; i < $numbers.length; i++){
     $form.classList.add('visible');
     this.parentNode.classList.add('done');
     this.classList.add('selected');
-    hrefParts[this.getAttribute('data-position')].isSelected = true;
+    hrefParts[this.getAttribute('data-position')].isS = true;
     constantHref = splitBySelected(hrefParts);
     $from.value = this.textContent;
     $to.value = parseInt(this.textContent, 10) + loadShift;
@@ -81,3 +88,8 @@ $form.addEventListener('submit', function(e){
   e.preventDefault();
   createGal($from.value, $to.value);
 });
+
+
+
+
+})(document, window)
