539a0353ab11ad75bd654f82041d373b3310ddb0
DrummerHead <drummerhead@gmail.com>
2014-02-02 10:01:45 -0200

    Don't zoom past container width, case insensitive regex, optimize for minification

--- examples/galFly.js
+++ examples/galFly.js
@@ -5,13 +5,13 @@
 /* -  galFly                                                    - *\
 |* -  Render linked images from current page, move with j & k,  - *|
 |* -  increase image size by clicking the image                 - *|
-\* -  v2.2                                                      - */
+\* -  v2.3                                                      - */
 
 
 
 
 var onlyLinks = Array.prototype.filter.call(document.querySelectorAll('a'), function(val, i, harry){
-    return /\.(jpe?g|gif|png|tiff|svg)$/.test(val.href);
+    return /\.(jpe?g|gif|png|tiff|svg)$/i.test(val.href);
   });
 
 var lis = Array.prototype.map.call(onlyLinks, function(val, i, harry){
@@ -21,16 +21,16 @@ var lis = Array.prototype.map.call(onlyLinks, function(val, i, harry){
 var stringified = JSON.stringify(lis).replace(/\\/g, '\\\\').replace(/'/g, '\\\'');
 
 
-var css = "body{margin:0;font-family:sans-serif;text-align:center}#vitrum{padding:1em;margin:0;list-style-type:none}#vitrum li{padding:4em 0}#vitrum img{display:block;max-width:100%;margin:0 auto;font-size:3em;text-align:center;color:#ccc}#vitrum a{display:inline-block;padding:1em;text-decoration:none;color:#ccc}#vitrum a:hover{color:#808080;text-decoration:underline}#move{position:fixed;bottom:1em;left:1em;padding:0;margin:0;font-size:1.4em;list-style-type:none;color:#888}#up,#down{cursor:pointer;padding:.2em}#load-more{display:block;padding:1em;cursor:pointer;background-color:#eee;color:#888;font-size:1.5em;font-weight:bold}#load-more:hover{background-color:#ddd;color:#777}";
+var css = "body{margin:0;font-family:sans-serif;text-align:center}#gal{padding:1em;margin:0;list-style-type:none}#gal li{padding:4em 0}#gal img{display:block;max-width:100%;margin:0 auto;font-size:3em;text-align:center;color:#ccc}#gal a{display:inline-block;padding:1em;text-decoration:none;color:#ccc}#gal a:hover{color:#808080;text-decoration:underline}#move{position:fixed;bottom:1em;left:1em;padding:0;margin:0;font-size:1.4em;list-style-type:none;color:#888}#up,#down{cursor:pointer;padding:.2em}#load-more{display:block;padding:1em;cursor:pointer;background-color:#eee;color:#888;font-size:1.5em;font-weight:bold}#load-more:hover{background-color:#ddd;color:#777}";
 
 // var imageLinks = JSON.parse('" + stringified +"');
 // var script = "
-var script = "var $gal=document.getElementById('vitrum');var $up=document.getElementById('up');var $down=document.getElementById('down');var imageLinks=JSON.parse('"+stringified+"');var shift=25;var increase=shift;var currentImage=0;var minImage=0;var maxImage=shift;var goToImage=function(id){if(id>=maxImage){window.scrollTo(0,77777);}else if(id>=minImage){var el=document.getElementById('a'+id);window.scrollTo(0,el.offsetTop-5);}};var imagesResize=function(imgs){for(var i=0;i<imgs.length;i++){var I=imgs.item(i);I.clickedTimes=0;I.addEventListener('click',function(el){var J=el.currentTarget;J.clickedTimes++;J.setAttribute('style','width: '+J.naturalWidth*(J.clickedTimes+1)+'px; height: '+J.naturalHeight*(J.clickedTimes+1)+'px;');if(J.clickedTimes==3){J.clickedTimes=-1;}});}};var inject=function(start){$gal.innerHTML='';var load='';currentImage=start;minImage=start;if(imageLinks.length>increase){for(var i=start;i<start+shift;i++){load+=imageLinks[i];}increase+=shift;maxImage=start+shift;$gal.insertAdjacentHTML('afterEnd','<strong id=\"load-more\">Load more</strong>');document.getElementById('load-more').addEventListener('click',function(){window.scrollTo(0,0);this.outerHTML='';inject(start+shift);});}else{for(var i=start;i<imageLinks.length;i++){load+=imageLinks[i];}maxImage=imageLinks.length;}$gal.innerHTML=load;imagesResize(document.querySelectorAll('img'));};window.onkeypress=function(e){if(e.charCode==106){goToImage(++currentImage);}else if(e.charCode==107){goToImage(--currentImage);}};up.addEventListener('click',function(){goToImage(--currentImage);});down.addEventListener('click',function(){goToImage(++currentImage);});inject(0);";
+var script ='!function(t,e){var n=function(t){return e.getElementById(t)},r=n("gal"),o=n("up"),i=n("down"),a=parseInt(t.getComputedStyle(r).getPropertyValue("width"),10),l=JSON.parse(\''+stringified+'\'),c=25,s=c,d=0,u=0,f=c,h=function(e){if(e>=f)t.scrollTo(0,77777);else if(e>=u){var r=n("a"+e);t.scrollTo(0,r.offsetTop-5)}},m=function(t){for(var e=0;e<t.length;e++){var n=t.item(e);n.ct=0,n.addEventListener("click",function(t){var e=t.currentTarget;e.ct++;var n=e.naturalWidth*(e.ct+1);a>=n?e.setAttribute("style","width:"+n+"px;height:"+e.naturalHeight*(e.ct+1)+"px"):e.setAttribute("style","width:100%"),3==e.ct&&(e.ct=-1)})}},p=function(o){r.innerHTML="";var i="";if(d=o,u=o,l.length>s){for(var a=o;o+c>a;a++)i+=l[a];s+=c,f=o+c,r.insertAdjacentHTML("afterEnd",\'<strong id="load-more">Load more</strong>\'),n("load-more").addEventListener("click",function(){t.scrollTo(0,0),this.outerHTML="",p(o+c)})}else{for(var a=o;a<l.length;a++)i+=l[a];f=l.length}r.innerHTML=i,m(e.querySelectorAll("img"))};t.onkeypress=function(t){106==t.charCode?h(++d):107==t.charCode&&h(--d)},o.addEventListener("click",function(){h(--d)}),i.addEventListener("click",function(){h(++d)}),p(0)}(window,document)';
 // ";
 
-var html = "<!doctype html><html><head><title>Image gallery</title><meta charset='utf-8'><style>" +
+var html = "<!doctype html><html><head><title>galFly</title><meta charset='utf-8'><style>" +
   css +
-  "</style></head><body><ul id='vitrum'>" +
+  "</style></head><body><ul id='gal'>" +
   "</ul><ol id='move'><li id='up'>&#9650;</li><li id='down'>&#9660;</li></ol><script>" +
   script +
   "</script></body></html>";
@@ -44,15 +44,15 @@ body {
   font-family: sans-serif;
   text-align: center;
 }
-#vitrum {
+#gal {
   padding: 1em;
   margin: 0;
   list-style-type: none;
 }
-#vitrum li {
+#gal li {
   padding: 4em 0
 }
-#vitrum img {
+#gal img {
   display: block;
   max-width: 100%;
   margin: 0 auto;
@@ -60,13 +60,13 @@ body {
   text-align: center;
   color: #ccc;
 }
-#vitrum a {
+#gal a {
   display: inline-block;
   padding: 1em;
   text-decoration: none;
   color: #ccc;
 }
-#vitrum a:hover {
+#gal a:hover {
   color: #808080;
   text-decoration: underline;
 }
@@ -102,9 +102,16 @@ body {
 
 
 /* * /
-var $gal = document.getElementById('vitrum');
-var $up = document.getElementById('up');
-var $down = document.getElementById('down');
+(function(window, document){
+
+
+var $ = function(element){
+  return document.getElementById(element)
+};
+var $gal = $('gal');
+var $up = $('up');
+var $down = $('down');
+var galWidth = parseInt(window.getComputedStyle($gal).getPropertyValue('width'), 10);
 var imageLinks = JSON.parse('');
 var shift = 25;
 var increase = shift;
@@ -114,10 +121,10 @@ var maxImage = shift;
 
 var goToImage = function(id){
   if(id >= maxImage){
-    window.scrollTo(0, 77777777777);
+    window.scrollTo(0, 77777);
   }
   else if(id >= minImage){
-    var el = document.getElementById('a' + id);
+    var el = $('a' + id);
     window.scrollTo(0, el.offsetTop - 5);
   }
 };
@@ -125,13 +132,19 @@ var goToImage = function(id){
 var imagesResize = function(imgs){
   for(var i = 0; i < imgs.length; i++){
     var I = imgs.item(i);
-    I.clickedTimes = 0;
+    I.ct = 0;
     I.addEventListener('click', function(el){
       var J = el.currentTarget;
-      J.clickedTimes++;
-      J.setAttribute('style', 'width: ' + J.naturalWidth * (J.clickedTimes + 1) + 'px; height: ' + J.naturalHeight * (J.clickedTimes + 1) + 'px;');
-      if(J.clickedTimes == 3){
-        J.clickedTimes = -1;
+      J.ct++;
+      var newWidth = J.naturalWidth * (J.ct + 1);
+      if(newWidth <= galWidth){
+        J.setAttribute('style', 'width:' + newWidth + 'px;height:' + J.naturalHeight * (J.ct + 1) + 'px');
+      }
+      else{
+        J.setAttribute('style', 'width:100%');
+      }
+      if(J.ct == 3){
+        J.ct = -1;
       }
     });
   }
@@ -151,7 +164,7 @@ var inject = function(start){
     maxImage = start + shift;
 
     $gal.insertAdjacentHTML('afterEnd', '<strong id=\"load-more\">Load more</strong>');
-    document.getElementById('load-more').addEventListener('click', function(){
+    $('load-more').addEventListener('click', function(){
       window.scrollTo(0, 0);
       this.outerHTML = '';
       inject(start + shift);
@@ -177,14 +190,17 @@ window.onkeypress = function(e){
   }
 };
 
-up.addEventListener('click', function(){
+$up.addEventListener('click', function(){
   goToImage(--currentImage);
 });
-down.addEventListener('click', function(){
+$down.addEventListener('click', function(){
   goToImage(++currentImage);
 });
 
 inject(0);
+
+
+})(window, document)
 /* */
 
 
